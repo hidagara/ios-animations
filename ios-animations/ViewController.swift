98bb4985c06b2f402669fa22b6f3dcdf43cce086
hidagara <hidagara@gmail.com>
2020-02-14 07:43:54 +0300

    add collectionview

--- ios-animations/ViewController.swift
+++ ios-animations/ViewController.swift
@@ -8,13 +8,83 @@
 
 import UIKit
 
+struct AnimationInfo {
+  let name: String
+}
 class ViewController: UIViewController {
+  var collectionView: UICollectionView!
 
+  var animationList = [AnimationInfo]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.view.translatesAutoresizingMaskIntoConstraints = false
     // Do any additional setup after loading the view.
+    animationList.append(AnimationInfo(name: "Move View Animations"))
+    setupCollectionView()
   }
 
+  private func setupCollectionView() {
+    let layout = UICollectionViewFlowLayout()
+    layout.itemSize = CGSize(width: 100, height: 100)
+    layout.sectionInset = UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10)
+    collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView.dataSource = self
+    self.view.addSubview(collectionView)
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    collectionView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+    collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+    collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 50).isActive = true
+    collectionView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+    collectionView.register(AnimationCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+    collectionView.backgroundColor = .green
+  }
+  
+  
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! AnimationCollectionViewCell
+    cell.setupCell(with: animationList[indexPath.row])
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return animationList.count
+  }
+}
+
+class AnimationCollectionViewCell: UICollectionViewCell {
+  var nameLabel: UILabel!
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.setupCellView()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupCellView() {
+    layer.cornerRadius = 8
+    layer.backgroundColor = UIColor.white.cgColor
+    nameLabel = UILabel(frame: CGRect(x: 5, y: 5, width: 100, height: 50))
+    nameLabel.translatesAutoresizingMaskIntoConstraints = false
+   
+    addSubview(nameLabel)
+     nameLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    nameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    nameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5).isActive = true
+    nameLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5).isActive = true
+    nameLabel.numberOfLines = 0
+    nameLabel.font = .monospacedDigitSystemFont(ofSize: 14, weight: .black)
+    nameLabel.textAlignment = .center
+  }
+  
+  func setupCell(with animationInfo: AnimationInfo) {
+    nameLabel.text = animationInfo.name
+    nameLabel.sizeToFit()
+  }
+}
